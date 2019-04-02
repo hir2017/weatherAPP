2b6bdc43902cea423e5dd9db887906ade1ed3551
john.ding <john.ding@asiainnovations.com>
2019-04-02 11:57:31 +1100

    click and mark

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,23 +23,37 @@ export class AppComponent implements OnInit {
   }
 
   getMap() {
-
-    // const map = new AMap.Map('iCenter');
-    // let geolocation;
-    // map.plugin('AMap.Geolocation', function() {
-    //   geolocation = new AMap.Geolocation();
-    // });
-
-    // geolocation.getCityInfo((status, result) => {
-    //   this.city = result;
-    //   this.city.name = result.city;
-    // });
-
     const map = new AMap.Map('container', {
       resizeEnable: true,
       zoom: 9,
-      // center: [116.407394, 39.904202]
-      center: [ -0.118092, 51.509865]
+      center: [116.407394, 39.904202]
+      // center: [ -0.118092, 51.509865]
+    });
+    map.on('complete', function() {
+      this.marker = new AMap.Marker({
+        icon: 'https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png',
+        position: [116.405467, 39.907761]
+      });
+      this.circle = new AMap.Circle({
+        center: new AMap.LngLat('116.403322', '39.920255'), // 圆心位置
+        radius: 1000,
+        strokeColor: '#F33',
+        strokeOpacity: 1,
+        strokeWeight: 3,
+        fillColor: '#ee2200',
+        fillOpacity: 0.35
+      });
+      map.add(this.marker);
+      map.add([this.marker, this.circle]);
+    });
+
+    map.on('click', function(ev) {
+      const lnglat = ev.lnglat;
+      const marker = new AMap.Marker({
+        icon: 'https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png',
+        position: [lnglat.getLng(), lnglat.getLat()]
+      });
+      map.add(marker);
     });
   }
 }
